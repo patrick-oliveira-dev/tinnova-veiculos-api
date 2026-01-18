0f9751aa407bc96bc51aad72abf821a4ab6ba578
Patrick <patrick.oliveira.dev@gmail.com>
2026-01-17 22:14:21 -0300

    tesfixmpAjuste swagger

--- src/main/java/com/tinnova/veiculos/config/OpenApiConfig.java
+++ src/main/java/com/tinnova/veiculos/config/OpenApiConfig.java
@@ -1,15 +1,17 @@
 package com.tinnova.veiculos.config;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Configuration
@@ -20,7 +22,7 @@ public class OpenApiConfig {
 
         final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI()
+        return new OpenAPI().servers( List.of( new Server().url( "http://localhost:8080" ).description( "Local Server" ) ) )
             .info(
                 new Info().title( "Veículos API" ).version( "1.0" ).description( "API REST para gerenciamento de veículos - Teste Tinnova" )
                     .contact( new Contact().name( "Seu Nome" ).email( "seu.email@example.com" ) ) )
