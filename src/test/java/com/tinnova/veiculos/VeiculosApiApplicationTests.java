68af5e8b26f4373202cf5226274b1d2d9680bb5a
Patrick <patrick.oliveira.dev@gmail.com>
2026-01-17 21:33:57 -0300

    first commit

---
+++ src/test/java/com/tinnova/veiculos/VeiculosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinnova.veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
